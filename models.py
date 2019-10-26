1ad7c48897ba0c4f021db8dced0468b5ac6ff251
zanenkn <zane.neikena@gmail.com>
2019-10-26 18:40:18 +0200

    Adds db columns zoom, lat and lng to Path model. Migrates the db.

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-
 from app import db
 
 
@@ -6,5 +5,21 @@ class Path(db.Model):
     __tablename__ = 'paths'
     id = db.Column(db.Integer, primary_key=True)
     svg = db.Column(db.Text, nullable=False)
+    zoom = db.Column(db.Float, nullable=False)
+    lat = db.Column(db.Integer, nullable=False)
+    lng = db.Column(db.Integer, nullable=False)
 
+    def __repr__(self):
+        return '<Path %r>' % self.id
 
+    def __init__(self, svg):
+        self.svg = svg
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'svg': self.svg,
+            'zoom': self.zoom,
+            'lat': self.lat,
+            'lng': self.lng
+        }
